111bcac34ddb4808980a89e5e61ebe61307b510c
Stefan Petkov <spp.bg@abv.bg>
2023-04-13 14:47:10 +0300

    end of Repository testing

--- src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -2,10 +2,28 @@ package net.javaguides.springboot.repository;
 
 import net.javaguides.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByEmail(String email);
+
+    //define custom query using JPQL with index parameters
+    @Query("select e from Employee e where e.firstName = ?1 and e.lastName = ?2")
+    Employee findBYJPQL(String firstName, String lastName);
+
+    //define custom query using JPQL with named parameters
+    @Query("select e from Employee e where e.firstName =:firstName and e.lastName =:lastName")
+    Employee findBYJPQLNamedParams(@Param("firstName") String firstName,@Param("lastName") String lastName);
+
+    //define custom query using Native SQL with index parameters
+    @Query(value = "select * from employees e where e.first_name =?1 and e.last_name =?2", nativeQuery = true)
+    Employee findByNativeSQL(String firstName, String lastName);
+
+    //define custom query using Native SQL with index parameters
+    @Query(value = "select * from employees e where e.first_name =:firstName and e.last_name =:lastName", nativeQuery = true)
+    Employee findByNativeSQLWithNamedParams(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
